33722f68968e2dc605b0b9ef7c743ca8268bb3fe
Kole Soto <kolesoto@Monnies-MacBook-Pro.local>
2015-12-24 15:05:14 -0600

    new xcode project to test git repositories made :)

---
+++ xcode-git-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xcode-git-test
+//
+//  Created by Kole Soto on 12/24/15.
+//  Copyright © 2015 Kole Soto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ xcode-git-testTests/xcode_git_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  xcode_git_testTests.swift
+//  xcode-git-testTests
+//
+//  Created by Kole Soto on 12/24/15.
+//  Copyright © 2015 Kole Soto. All rights reserved.
+//
+
+import XCTest
+@testable import xcode_git_test
+
+class xcode_git_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
